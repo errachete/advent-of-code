dd9bc4b136bae1a33130591f8e07e653f12dc3cd
errachete <rafaelherrer@hotmail.es>
2024-12-01 16:13:22 +0000

    Day 1 / 2024

---
+++ 2024/day1/1a.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main()
+{
+    vector<int> list1;
+    vector<int> list2;
+
+    // Read both lists
+    int aux;
+    ifstream in("input.txt");
+    while (in >> aux)
+    {
+        list1.push_back(aux);
+        in >> aux;
+        list2.push_back(aux);
+    }
+    in.close();
+
+    // Sort the lists
+    sort(list1.begin(), list1.end());
+    sort(list2.begin(), list2.end());
+
+    // Iterate the lists adding the differences between the two numbers
+    int total = 0;
+    for (int i = 0; i < list1.size(); ++i)
+        total += abs(list1[i] - list2[i]);
+
+    cout << "Result: " << total << endl;
+}
---
+++ 2024/day1/1b.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+
+int main()
+{
+    // Two maps containing <number, appearances>
+    unordered_map<int,int> counts1;
+    unordered_map<int,int> counts2;
+
+    // Read both lists counting the number of times each number appears
+    int aux;
+    ifstream in("input.txt");
+    while (in >> aux)
+    {
+        if (!counts1.count(aux))
+            counts1[aux] = 0;
+        ++counts1[aux];
+
+        in >> aux;
+
+        if (!counts2.count(aux))
+            counts2[aux] = 0;
+        ++counts2[aux];
+    }
+    in.close();
+
+    // Iterate the first map checking count of the number in both
+    int total = 0;
+    for (auto a : counts1)
+        if (counts2.count(a.first))
+            total += a.first * counts2[a.first] * a.second;
+
+    cout << "Result: " << total << endl;
+}
